b6d8bb2f7d48aa9c97611aa2da60d8c7734d7dcd
Erick Jung <erickjung@gmail.com>
2016-06-02 22:18:48 -0300

    first ver

--- js/app.js
+++ js/app.js
@@ -1,33 +1,41 @@
 var App = {
     help: function () {
-        this.echo("Available commands:");
-        this.echo("\tcowsay");
-        this.echo("\tabout");
-        this.echo("\thelp");
+        this.echo("\nAvailable commands:");
+        this.echo("\tprojects -> list with personal projects");
+        this.echo("\tcowsay   -> generates ASCII pictures with a message");
+        this.echo("\tabout    -> about myself");
+        this.echo("\thelp     -> this screen");
         this.echo("");
+    },
+    projects: function () {
+        this.echo("\nsoon!!!\n")
     },
     cowsay: function (text) {
         this.echo(cowsay(text))
     },
     about: function () {
-        this.echo("hellows!!!.", {raw: true});
-    },
-    id: function () {
-        this.echo("uid=1000(tui) gid=1000(tui)");
+        this.echo("\nChoose the best way to know or contact me. :)\n");
+        this.echo("-> <a href='mailto:erickjung@gmail.com'>email</a>\n", {raw: true});
+        this.echo("-> <a href='https://github.com/erickjung' target='_blank'>github</a>\n", {raw: true});
+        this.echo("-> <a href='https://linkedin.com/in/erickjung' target='_blank'>linkedin</a>\n", {raw: true});
+        this.echo("-> <a href='https://twitter.com/erickjung' target='_blank'>twitter</a>\n", {raw: true});
+        this.echo("-> <a href='https://medium.com/@erickjung' target='_blank'>medium</a>\n", {raw: true});
+        this.echo("\n");
     }
 }
 
 jQuery(document).ready(function ($) {
     $('body').terminal(App, {
-        greetings: "     __                      \n"+
-    "    |__|__ __  ____    ____  \n"+
-    "    |  |  |  \\/    \\  / ___\\ \n"+
-    "    |  |  |  /   |  \\/ /_/  >\n"+
-    "/\\__|  |____/|___|  /\\___  / \n"+
-    "\\______|          \\//_____/\n",
+        greetings: cowsay("yo! welcome!!") +
+        "\n\n\n" +
+        "-- erickjung.com --\n" +
+        ">> Initializing system... [[[b;#44D544;]DONE]]\n" +
+        ">> Detecting user ip... [[[b;#44D544;]DONE]]\n" +
+        ">> Logging as guest\n\n" +
+        "Type [[b;#44D544;]help] to get a list of available commands.\n\n",
         prompt: function (p) {
             var path = '~'
-            p("127.0.0.1:" + path + "$ ");
+            p(myip + ":" + path + "$ ");
         },
         onBlur: function () {
             return false;
